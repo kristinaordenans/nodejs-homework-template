959850dd7ceebe25f4a867bf59b495546aed1871
kristinaordenans <okristin92@gmail.com>
2023-08-11 20:04:59 +0300

    all

--- models/contacts.js
+++ models/contacts.js
@@ -1,16 +1,52 @@
 // const fs = require('fs/promises')
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models","contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    return result || null;}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === contactId)
+    if(index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async ({name,email,phone}) => {
+  const contacts = await listContacts();
+    const newContactItem = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+    }
+  contacts.push(newContactItem);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContactItem;
+}
 
-module.exports = {
+const updateContact = async (contactId, {name, email, phone}) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, name, email, phone };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index]
+}
+
+export default {
   listContacts,
   getContactById,
   removeContact,
