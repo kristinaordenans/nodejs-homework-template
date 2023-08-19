4a3b2a8b50642aa864fbb8224b7f6ae1d97a3a4d
kristinaordenans <okristin92@gmail.com>
2023-08-19 23:45:53 +0300

    mistake user.token

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -21,7 +21,6 @@ const getContactById = async (req, res) => {
 }
 
 const addContact = async (req, res) => { 
-  console.log(req.user)
   const { _id: owner } = req.user;
     const result = await Contact.create({...req.body, owner });
     res.status(201).json(result);
